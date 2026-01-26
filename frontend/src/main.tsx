087ac39f3dd3a076af0920c76ae1fb2ecb2c19c5
FelixMeentken <felix.meentken@boc-ch.com>
2026-01-26 15:09:07 +0100

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,14 @@
+// --- FIX START: Polyfill für HTTP (Muss ganz oben stehen!) ---
+if (typeof crypto !== 'undefined' && !crypto.randomUUID) {
+  // @ts-ignore
+  crypto.randomUUID = () => {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c: any) =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  };
+}
+// --- FIX ENDE ---
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
@@ -13,4 +24,5 @@ createRoot(container).render(
     <App />
   </StrictMode>
 );
+
 
